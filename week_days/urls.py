fd65a4274991edac13d0298099ebd4806d388ac7
Kiryl Dumanski <Kocherizhkin@gmail.com>
2022-09-28 07:53:27 +0300

    Added the ability to display information about the todo-list by the ordinal day of the week

--- week_days/urls.py
+++ week_days/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('<str:day_of_week>', views.todo_list),
+    path('<int:day_of_week>', views.todo_list_by_number),
+    path('<str:day_of_week>', views.todo_list, name='todo-list'),
 ]
--- week_days/views.py
+++ week_days/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+from django.urls import reverse
+
+
 def index(request):
     return HttpResponse('Вы попали на страницу приложения Week days')
 
@@ -22,3 +23,11 @@ def todo_list(request, day_of_week: str):
     if day_of_week in days_of_week:
         return HttpResponse(f'Список дел на {day_of_week}')
     return HttpResponse(f'Неверный день недели - {day_of_week}')
+
+
+def todo_list_by_number(request, day_of_week: int):
+    if day_of_week not in range(1, 8):
+        return HttpResponse(f'Неверный порядковый номер недели - {day_of_week}')
+    day = days_of_week[day_of_week - 1]
+    redirect_url = reverse('to-do-list', args=[day])
+    return HttpResponseRedirect(redirect_url)
