2b92dbc819864046651a686632e16c3de913f464
Kiryl Dumanski <Kocherizhkin@gmail.com>
2022-09-28 17:30:59 +0300

    Added navbar.html. Changes in the structure of html files in the horoscope app.

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('types', views.index_types),
+    path('', views.index, name='horoscope-index'),
+    path('types', views.index_types, name='types-index'),
     path('types/<str:type_zodiac>', views.zodiacs_by_type, name='zodiacs-by-type'),
     path('<int:zodiac_sign_number>', views.get_info_about_zodiac_by_number),
     path('<int:month>/<int:day>', views.get_info_about_zodiac_by_month_and_day),
