910a9b1b281f3c12cf0a6e7cc7ade1554579e2fb
Kiryl Dumanski <Kocherizhkin@gmail.com>
2022-09-28 07:40:21 +0300

    Added the ability to display information about the signs of the zodiac by the ordinal number of the sign

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('<sign_zodiac>', views.get_info_about_zodiac),
+    path('<int:zodiac_sign_number>', views.get_info_about_zodiac_by_number),
+    path('<str:sign_zodiac>', views.get_info_about_zodiac, name='zodiac-info'),
 ]
--- horoscope/views.py
+++ horoscope/views.py
@@ -1,19 +1,20 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-zodiac_list = [
-    'aries',
-    'taurus',
-    'gemini',
-    'cancer',
-    'leo',
-    'virgo',
-    'libra',
-    'scorpio',
-    'sagittarius',
-    'capricorn',
-    'aquarius',
-    'pisces'
-]
+zodiac_dict = {
+    'aries': 'Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).',
+    'taurus': 'Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).',
+    'gemini': 'Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).',
+    'cancer': 'Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).',
+    'leo': 'Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа).',
+    'virgo': 'Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).',
+    'libra': 'Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).',
+    'scorpio': 'Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).',
+    'sagittarius': 'Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря).',
+    'capricorn': 'Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).',
+    'aquarius': 'Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).',
+    'pisces': 'Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта).'
+}
 
 
 # Create your views here.
@@ -22,6 +23,16 @@ def index(request):
 
 
 def get_info_about_zodiac(request, sign_zodiac: str):
-    if sign_zodiac in zodiac_list:
-        return HttpResponse(f'Гороскоп для {sign_zodiac}')
+    description = zodiac_dict.get(sign_zodiac)
+    if description:
+        return HttpResponse(description)
     return HttpResponse(f'Неверный знак зодиака {sign_zodiac}')
+
+
+def get_info_about_zodiac_by_number(request, zodiac_sign_number: int):
+    zodiacs = list(zodiac_dict)
+    if zodiac_sign_number > len(zodiacs):
+        return HttpResponse(f'Неправильный порядковый номер зодиака - {zodiac_sign_number}')
+    zodiac_name = zodiacs[zodiac_sign_number - 1]
+    redirect_url = reverse('zodiac-info', args=[zodiac_name])
+    return HttpResponseRedirect(redirect_url)
